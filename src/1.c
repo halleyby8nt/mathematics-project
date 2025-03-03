9315610b374eb53f81c3796674bef77e7e4f2ab5
halleyby8nt <halleyby8nt@gmx.com>
2025-03-03 13:06:13 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,16 @@
+// Random C code
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int a = 5;
+    float b = 3.14;
+    char c = 'a';
+
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %f\n", b);
+    printf("The value of c is %c\n", c);
+
+    return 0;
+}
